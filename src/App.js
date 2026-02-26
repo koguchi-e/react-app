e90561df808fb05e37c2dbb1e6ccc6f4167a21a8
koguchi-e <koguchi.e915@gmail.com>
2026-02-26 11:21:34 +0900

    3-24

--- src/App.js
+++ src/App.js
@@ -1,20 +1,80 @@
-import { useState } from "react";
+import { Component } from "react";
+import "./App.css";
 
-export default function App() {
-  const [count, setCount] = useState(0);
-  const [inVisible, setIsVisible] = useState(true);
+// 3-23
+class App extends Component {
+  data = ["This is list sample.", "test list", "converting list to array"];
 
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>EXAMPLE</h2>
-      <p>現在のカウント{count}</p>
-      <button onClick={() => setCount(count + 1)}>+1 する</button>
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: this.data,
+    };
+    // this.doAction = this.doAction.bind(this);
+  }
 
-      <hr />
-      <button onClick={() => setIsVisible(!setIsVisible)}>
-        表示の切り替え
-      </button>
-      {inVisible && <p>この文章は表示/非表示が切り替わります</p>}
-    </div>
-  );
+  // doAction(e) {
+  //   let x = e.pageX;
+  //   let y = e.pageY;
+  //   this.data.push({ x: x, y: y });
+  //   this.setState({
+  //     list: this.data,
+  //   });
+  // }
+
+  render() {
+    return (
+      <div>
+        <h1 className="bg-primary text-white display4">React</h1>
+        <div className="container">
+          <p className="subtitle">draw rectangle</p>
+          <List title="sample list" data={this.data}></List>
+        </div>
+      </div>
+    );
+  }
 }
+
+class List extends Component {
+  number = 1;
+  render() {
+    let data = this.props.data;
+    return (
+      <div>
+        <p className="h5 text-center">{this.props.title}</p>
+        <ul className="list-group">
+          {data.map((item, key) => (
+            <li className="lis-group-item" key={key}>
+              <Item number={key + 1} value={item}></Item>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Item extends Component {
+  itm = {
+    fontSize: "16pt",
+    color: "#00f",
+    textDecoration: "underline",
+    textDecorationColor: "#ddf",
+  };
+
+  num = {
+    fontWeight: "bold",
+    color: "red",
+  };
+
+  render() {
+    return (
+      <p style={this.item}>
+        <span style={this.num}>[{this.props.number}]&nbsp;</span>
+        {this.props.value}
+      </p>
+    );
+  }
+}
+
+export default App;
